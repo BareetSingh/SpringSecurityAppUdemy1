56af6acff42ffffae1c0c39e1c205fef6171c0a4
Bareet Singh <singhbareet@gmail.com>
2022-09-27 23:03:41 +0530

    Configuring users using InMemoryUserDetailsManager

--- src/main/java/com/app/springsecurity/config/ProjectSecurityConfiguration.java
+++ src/main/java/com/app/springsecurity/config/ProjectSecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.app.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -23,12 +28,26 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("12345")
+//                .authorities("ADMIN")
+//                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("12345")
-                .authorities("ADMIN")
-                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("admin").password("12345").authorities("ADMIN").build();
+        UserDetails user2 = User.withUsername("user").password("12345").authorities("READ").build();
+        userDetailsService.createUser(user1);
+        userDetailsService.createUser(user2);
+        auth.userDetailsService(userDetailsService);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
