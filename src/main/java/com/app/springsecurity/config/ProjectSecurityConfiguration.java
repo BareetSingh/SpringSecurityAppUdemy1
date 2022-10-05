88559f4c2cb712916a24d2084c8773435d6a0dbd
Bareet Singh <singhbareet@gmail.com>
2022-10-05 15:18:34 +0530

    Using JdbcUserDetailsManager to perform authentication

--- src/main/java/com/app/springsecurity/config/ProjectSecurityConfiguration.java
+++ src/main/java/com/app/springsecurity/config/ProjectSecurityConfiguration.java
@@ -2,14 +2,14 @@ package com.app.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -36,14 +36,19 @@ public class ProjectSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
 //    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-        UserDetails user1 = User.withUsername("admin").password("12345").authorities("ADMIN").build();
-        UserDetails user2 = User.withUsername("user").password("12345").authorities("READ").build();
-        userDetailsService.createUser(user1);
-        userDetailsService.createUser(user2);
-        auth.userDetailsService(userDetailsService);
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//        UserDetails user1 = User.withUsername("admin").password("12345").authorities("ADMIN").build();
+//        UserDetails user2 = User.withUsername("user").password("12345").authorities("READ").build();
+//        userDetailsService.createUser(user1);
+//        userDetailsService.createUser(user2);
+//        auth.userDetailsService(userDetailsService);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
