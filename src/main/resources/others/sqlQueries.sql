2afe8baafe91b58282b3e52291c0c90c68acd4fc
Bareet Singh <singhbareet@gmail.com>
2022-10-08 13:30:07 +0530

    Implementing and enhancing our application to use Bcrypt password encoder

--- src/main/resources/others/sqlQueries.sql
+++ src/main/resources/others/sqlQueries.sql
@@ -2,10 +2,10 @@
 create table customer (
 id serial not null primary key,
 email varchar(45) not null,
-pwd varchar(45) not null,
+pwd varchar(200) not null,
 role varchar(45) not null
 );
 
 INSERT INTO public.customer
 (email, pwd, "role")
-VALUES('test@gmail.com', '123456', 'ADMIN');
+VALUES('test@gmail.com', '$2a$12$YYD4GU311sIofQZzEtScheaNSkJFskGgmZ7RFauwoqdDcS9Oc/hcG', 'ADMIN');
